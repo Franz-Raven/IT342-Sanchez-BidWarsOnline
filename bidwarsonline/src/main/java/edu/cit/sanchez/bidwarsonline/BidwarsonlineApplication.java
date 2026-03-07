beac363b03261a1440683c57c7787de7e59e1c87
LAB13_USER ON WS29 <L13Y09W29@LabHCI.net>
2026-03-07 08:47:18 +0800

    Initial Commit

---
+++ bidwarsonline/src/main/java/edu/cit/sanchez/bidwarsonline/BidwarsonlineApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.sanchez.bidwarsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidwarsonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidwarsonlineApplication.class, args);
+	}
+
+}
---
+++ bidwarsonline/src/test/java/edu/cit/sanchez/bidwarsonline/BidwarsonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.sanchez.bidwarsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidwarsonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
